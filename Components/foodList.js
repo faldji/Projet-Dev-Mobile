cdf7055568b2886cc080995ae29fba11fed034db
Tibé Pak Nakote <32389782+Eoth@users.noreply.github.com>
2020-01-20 13:32:24 +0100

    foodList camelCase

--- Components/foodList.js
+++ Components/foodList.js
@@ -1,34 +1,31 @@
-const foodlist = [
+
+export const oodList = [
     {
       id: 1,
       name: 'Couscous Tajine',
       image: '../assets/img/foodLIist-couscous_tajine.jgp',
       description: 'Spécialié magrébine, très séculant qui met en valeur une culture très riche',
-      price: 13,
-      isBasket: true
+      price: 13
     },
     {
       id: 2,
       name: 'Poulet roti',
       image: '../assets/img/foodLIist-poulet_roti.jgep',
       description: 'Ah ! Le poulet, une magnifique viande qui te fait aimer ta langue',
-      price: 11,
-      isBasket: true
+      price: 11
     },
     {
       id: 3,  
       name: 'Fritte Poulet',
       image: '../assets/img/foodLIist-poulet_roti_frites.jgep',
       description: 'La spécialité belge qui vous aimer la clture belge accompagné d\'un poulet bien grillé',
-      price: 25,
-      isBasket: false
+      price: 25
     },
     {
       id: 4,
       name: 'Burger',
       image: '../assets/img/foodLIist-burger_maison.jgep',
       description: 'Rien à dire tu connnais déja',
-      price: 6,
-      isBasket: false
+      price: 6
     }
 ]
