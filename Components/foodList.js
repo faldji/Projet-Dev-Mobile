5bbe156dcbe5f4b9e66f68e0cf8baa0e09a8ed1e
Tibé Pak Nakote <32389782+Eoth@users.noreply.github.com>
2020-01-20 11:44:46 +0100

    add foodlist

---
+++ Components/foodList.js
@@ -0,0 +1,34 @@
+const foodlist = [
+    {
+      id: 1,
+      name: 'Couscous Tajine',
+      image: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      description: 'Spécialié magrébine, très séculant qui met en valeur une culture très riche',
+      price: 13,
+      isBasket: true
+    },
+    {
+      id: 2,
+      name: 'Poulet roti',
+      image: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      description: 'Ah ! Le poulet, une magnifique viande qui te fait aimer ta langue',
+      price: 11,
+      isBasket: true
+    },
+    {
+      id: 3,  
+      name: 'Fritte Poulet',
+      image: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      description: 'La spécialité belge qui vous aimer la clture belge accompagné d\'un poulet bien grillé',
+      price: 25,
+      isBasket: false
+    },
+    {
+      id: 4,
+      name: 'Burger',
+      image: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      description: 'Rien à dire tu connnais déja',
+      price: 6,
+      isBasket: false
+    }
+]
