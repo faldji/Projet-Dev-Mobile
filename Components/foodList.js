1beb66d701361653697b0305c03e9bd3c8160ac9
Tibé Pak Nakote <32389782+Eoth@users.noreply.github.com>
2020-01-20 13:57:35 +0100

    foodList camelCase

--- Components/foodList.js
+++ Components/foodList.js
@@ -5,6 +5,7 @@ export const FoodList = [
       name: 'Couscous Tajine',
       image: '../assets/img/foodLIist-couscous_tajine.jgp',
       description: 'Spécialié magrébine, très séculant qui met en valeur une culture très riche',
+      allergen: 'le lait de soja',
       price: 13
     },
     {
@@ -12,6 +13,7 @@ export const FoodList = [
       name: 'Poulet roti',
       image: '../assets/img/foodLIist-poulet_roti.jgep',
       description: 'Ah ! Le poulet, une magnifique viande qui te fait aimer ta langue',
+      allergen: 'le lait de soja',
       price: 11
     },
     {
@@ -19,6 +21,7 @@ export const FoodList = [
       name: 'Fritte Poulet',
       image: '../assets/img/foodLIist-poulet_roti_frites.jgep',
       description: 'La spécialité belge qui vous aimer la clture belge accompagné d\'un poulet bien grillé',
+      allergen: 'le lait de soja',
       price: 25
     },
     {
@@ -26,6 +29,7 @@ export const FoodList = [
       name: 'Burger',
       image: '../assets/img/foodLIist-burger_maison.jgep',
       description: 'Rien à dire tu connnais déja',
+      allergen: 'le lait de soja',
       price: 6
     }
 ]
