c0411ddc66ef5fac7572e79ba5a9d48560bae902
Tibé Pak Nakote <32389782+Eoth@users.noreply.github.com>
2020-01-20 13:18:44 +0100

    add picture

--- Components/foodList.js
+++ Components/foodList.js
@@ -2,7 +2,7 @@ const foodlist = [
     {
       id: 1,
       name: 'Couscous Tajine',
-      image: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      image: '../assets/img/foodLIist-couscous_tajine.jgp',
       description: 'Spécialié magrébine, très séculant qui met en valeur une culture très riche',
       price: 13,
       isBasket: true
@@ -10,7 +10,7 @@ const foodlist = [
     {
       id: 2,
       name: 'Poulet roti',
-      image: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      image: '../assets/img/foodLIist-poulet_roti.jgep',
       description: 'Ah ! Le poulet, une magnifique viande qui te fait aimer ta langue',
       price: 11,
       isBasket: true
@@ -18,7 +18,7 @@ const foodlist = [
     {
       id: 3,  
       name: 'Fritte Poulet',
-      image: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      image: '../assets/img/foodLIist-poulet_roti_frites.jgep',
       description: 'La spécialité belge qui vous aimer la clture belge accompagné d\'un poulet bien grillé',
       price: 25,
       isBasket: false
@@ -26,7 +26,7 @@ const foodlist = [
     {
       id: 4,
       name: 'Burger',
-      image: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      image: '../assets/img/foodLIist-burger_maison.jgep',
       description: 'Rien à dire tu connnais déja',
       price: 6,
       isBasket: false
