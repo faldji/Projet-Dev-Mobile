dc8aa7f0f764ba7c756c9f21b1c33a2495e663a1
Tibé Pak Nakote <32389782+Eoth@users.noreply.github.com>
2020-01-22 12:34:08 +0100

    HOME SWEET HOME

--- Screens/Success.js
+++ Screens/Success.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ImageBackground} from 'react-native';
 import MenuBar from "../Components/MenuBar";
 import {Styles} from "../assets/style/Styles";
 
@@ -13,16 +13,18 @@ class Success extends React.Component {
             <View style={Styles.container}>
                 <MenuBar leftElement="menu" onLeftElementPress={() => navigation.toggleDrawer()}
                          rightElement={"shopping-cart"} onRightElementPress={() => navigation.navigate('MyCart')}/>
-                <ImageBackground style={{width :192 , height:256}} source={require('../assets/img/valid.png')}>
-                    <View>
-                        <Text style ={{textAlign:'center', color:'#fff', fontSize:22}}> Commande Envoyé</Text>
+                <View>
+                    <ImageBackground style={{alignItems:'center', width :400 , height:300}} source={require('../assets/img/valid.png')}>
+                        <View style={{alignItems:'center', justifyContent:'center',flexDirection:'row'}}>
+                           <Text style ={{textAlign:'center', color:'#2E8B57', fontSize:22}}> Commande Envoyé </Text>
+                        </View>
+                    </ImageBackground>
+                <   View>
+                      <Text style ={{textAlign:'center', color:'#BDB76B', fontSize:20}}>Votre repas Copieux vous sera livré dès la fin de votre cours</Text>
                     </View>
-                </ImageBackground>
-                <View>
-                    <Text style ={{textAlign:'center', color:'#fff'}}>Votre repas Copieux vous sera livré dès la fin de votre cours</Text>
-                </View>
-                <View>
-                    <Text style ={{textAlign:'center', color:'#fff', fontSize:22}}> Il vous reste 30 &euro; sur la carte crous</Text>
+                    <View>
+                      <Text style ={{textAlign:'center', color:'#556B2F'}}> Il vous reste 30 &euro; sur la carte crous</Text>
+                   </View>
                 </View>
             </View>
         );
